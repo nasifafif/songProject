c5bcfc1f8ff71056197ca6532db369c2ac9dc6c6
nasif <nasifafif@gmail.com>
2025-04-07 21:39:53 -0400

    finally done

--- src/musicCollection.java
+++ src/musicCollection.java
@@ -13,46 +13,9 @@ public class musicCollection {
         menu();
     }
 
-    public void importData(){
-        try{
-            File myFile = new File("src\\songs_normalize.csv");
-            Scanner scanner = new Scanner(myFile);
-            while(scanner.hasNext()){
-                String data = scanner.nextLine();
-                String[] split = data.split(",");
-                String artist = split[0];
-                String songTitle = split[1];
-                int duration = 0;
-                try {
-                    duration = Integer.parseInt(split[2]);
-                } catch (NumberFormatException e) {
-                    System.out.print("");
-                }
-                int year = 0;
-                try {
-                    year = Integer.parseInt(split[3]);
-                } catch (NumberFormatException e) {
-                    System.out.print("");
-                }
-                String genre = split[4];
-                double dance_ability = 0.0;
-                try {
-                    dance_ability = Double.parseDouble(split[5]);
-                } catch (NumberFormatException e) {
-                    System.out.print("");
-                }
-                Song song;
-                if(genre.contains("rock")){
-                    song = new RockSong(artist,songTitle,duration,year,genre,dance_ability);
-                }else {
-                    song = new Song(artist, songTitle, duration, year, genre);
-                }
-                songs.add(song);
-
-            }
-        } catch (IOException exception) {
-            System.out.println(exception.getMessage());
-        }
+    public void importData() {
+        String filePath = "src\\songs_normalize.csv"; // Path to your CSV file
+        songs = importer.importData(filePath);
     }
 
 
